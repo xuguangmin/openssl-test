27ea5f2ef23404ef3c64df651b4abfcaa53614c3
xuguangmin <345253580@qq.com>
2017-12-02 18:31:03 +0800

    add file bio

---
+++ fileBio.c
@@ -0,0 +1,32 @@
+/* file bio*/
+
+#include <stdio.h>
+#include <openssl/bio.h>
+int main()
+{
+	BIO *b=NULL;
+	int len=0,outlen=0;
+	char *out=NULL;
+
+	b=BIO_new_file("bf.txt","w");
+	len=BIO_write(b,"openssl",4);
+	len=BIO_printf(b,"%s","zcp");
+	BIO_free(b);
+	b=BIO_new_file("bf.txt","r");
+	len=BIO_pending(b);
+	printf("len:%d\n", len);
+	len=50;
+	out=(char *)OPENSSL_malloc(len);
+	len=1;
+	while(len>0)
+	{
+		len=BIO_read(b,out+outlen,1);
+		outlen+=len;
+	}
+
+	printf("out:%s\n", out);
+
+	BIO_free(b);
+	free(out);
+	return 0;
+}
