68cf7953f07416bc69668b528559c9b2630f19fe
xuguangmin <345253580@qq.com>
2017-12-02 17:45:29 +0800

    add dso.c,动态库加载测试

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ openssl版本:OpenSSL 1.1.1-dev  xx XXX xxxx
 	stack.c 堆栈结构测试学习
 	lhash.c	哈希结构测试学习
 	mem_alloc.c  openssl内存分配函数测试
+	dso.c	测试openssl的动态库加载功能，DSO,测试用例有问题
---
+++ dso.c
@@ -0,0 +1,24 @@
+#include <openssl/dso.h>
+#include <openssl/bio.h>
+int main()
+{
+	DSO *d;
+	void (*f1)();
+	void (*f2)();
+	BIO *(*BIO_newx)(BIO_METHOD *a);
+	BIO *(*BIO_freex)(BIO_METHOD *a);
+	BIO *test;
+	d=DSO_new();
+	d=DSO_load(d,"libeay32",NULL,0);
+	f1=DSO_bind_func(d,"BIO_new");
+	//f2=DSO_bind_func(d,"BIO_free");
+	BIO_newx=(BIO *(*)(BIO_METHOD *))f1;
+	//BIO_freex=(BIO *(*)(BIO_METHOD *))f2;
+	test=BIO_newx(BIO_s_file());
+	return 0;
+	BIO_set_fp(test,stdout,BIO_NOCLOSE);
+	BIO_puts(test,"abd\n\n");
+	//BIO_freex(test);
+	DSO_free(d);
+	return 0;
+}
